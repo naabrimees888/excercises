906f603a73b8e9dc16d80c808231743e4c0c2c35
Andre Kose <naabrimees888@gmail.com>
2018-10-11 22:31:02 +0300

    Adding gitignore file

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -1,8 +1,20 @@
 package com.sda.git;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] array = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert 5 numbers: ");
+	    for (int index = 0; index<array.length; index++){
+            array[index] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int num : array){
+            sum = sum + num;
+        }
+        System.out.println("Sum of all array elemnts is: " + sum);
     }
 }
--- src/com/sda/git/Sum.java
+++ src/com/sda/git/Sum.java
@@ -1,17 +1,12 @@
 package com.sda.git;
 
+
 import java.util.Scanner;
 
 public class Sum {
-    int array = new int[]
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Please enter 5 numbers: ");
-        for (int i=0; i<array.Length; i++){
-            array[i] = scanner.nextInt();
-        }
-        int sum = 0;
-        for (int num : a){
-            sum = sum + num;
-        }
-        System.out.println("Sum of all array elemnts is: " + sum);
-    }
+    public int[] array = new int[5];
+
+//    for (int i = 0; i<array.lenght;i++){
+//
+//    }
+}
