b143ba1becbf4e3707492a7e92c6a7a60a4e12c7
Andre Kose <naabrimees888@gmail.com>
2018-10-12 12:46:09 +0300

    Level 1 excercise update.

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -4,17 +4,4 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int[] array = new int[5];
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Insert 5 numbers: ");
-	    for (int index = 0; index<array.length; index++){
-            array[index] = scanner.nextInt();
-        }
-        int sum = 0;
-        for (int num : array){
-            sum = sum + num;
-        }
-        System.out.println("Sum of all array elemnts is: " + sum);
-    }
 }
--- src/com/sda/git/Sum.java
+++ src/com/sda/git/Sum.java
@@ -4,9 +4,17 @@ package com.sda.git;
 import java.util.Scanner;
 
 public class Sum {
-    public int[] array = new int[5];
-
-//    for (int i = 0; i<array.lenght;i++){
-//
-//    }
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert 5 numbers: ");
+	    for (int index = 0; index<array.length; index++){
+            array[index] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int num : array){
+            sum = sum + num;
+        }
+        System.out.println("Sum of all array elemnts is: " + sum);
+    }
 }
